1679fb22126946d085e08e56141660a038fd85da
Hyun Yi <hy00un__@naver.com>
2021-01-18 04:14:43 +0900

    Add context menu actions (select address)

--- Hyara_IDA.py
+++ Hyara_IDA.py
@@ -3,18 +3,62 @@ import ida_kernwin
 import idaapi
 
 
+class Hyara_action_handler_t(idaapi.action_handler_t):
+    def __init__(self, widget):
+        idaapi.action_handler_t.__init__(self)
+        self.widget = widget
+
+    def activate(self, ctx):
+        self.widget.setText(hex(idaapi.get_screen_ea()))
+        return 1
+
+    def update(self, ctx):
+        return idaapi.AST_ENABLE_ALWAYS
+
+
+class Hooks(ida_kernwin.UI_Hooks):
+    def __init__(self):
+        ida_kernwin.UI_Hooks.__init__(self)
+
+    def finish_populating_widget_popup(self, widget, popup):
+        ida_kernwin.attach_action_to_popup(widget, popup, "Hyara:select_start_address", None)
+        ida_kernwin.attach_action_to_popup(widget, popup, "Hyara:select_end_address", None)
+
+
 class HyaraWidget(ida_kernwin.PluginForm):
     def OnCreate(self, form):
         self.parent = self.FormToPyQtWidget(form)
-
         self.HyaraIDA = HyaraIDA()
         self.parent.setLayout(self.HyaraIDA.layout)
 
+        idaapi.register_action(
+            ida_kernwin.action_desc_t(
+                "Hyara:select_start_address",
+                "Hyara - Select Start Address",
+                Hyara_action_handler_t(self.HyaraIDA._start_address),
+                "Ctrl+Shift+S",
+                "Hyara - Select Start Address",
+            )
+        ),
+
+        idaapi.register_action(
+            ida_kernwin.action_desc_t(
+                "Hyara:select_end_address",
+                "Hyara - Select End Address",
+                Hyara_action_handler_t(self.HyaraIDA._end_address),
+                "Ctrl+Shift+E",
+                "Hyara - Select End Address",
+            )
+        ),
+
     def OnClose(self, form):
-        pass
+        idaapi.unregister_action("Hyara:select_start_address")
+        idaapi.unregister_action("Hyara:select_end_address")
+        hooks.unhook()
 
 
 class HyaraPlugin(idaapi.plugin_t):
+    # https://www.hex-rays.com/products/ida/support/sdkdoc/group___p_l_u_g_i_n__.html
     flags = idaapi.PLUGIN_UNL
     comment = "Hyara"
     help = "help"
@@ -22,12 +66,16 @@ class HyaraPlugin(idaapi.plugin_t):
     wanted_hotkey = "Ctrl+Shift+Y"
 
     def init(self):
+        global hooks
         idaapi.msg("[*] Hyara Plugin\n")
+        hooks = Hooks()
+        hooks.hook()
         return idaapi.PLUGIN_OK
 
     def run(self, arg):
         plg = HyaraWidget()
         plg.Show("Hyara")
+
         try:
             widget_a = ida_kernwin.find_widget("IDA View-A")
             widget_Hyara = ida_kernwin.find_widget("Hyara")

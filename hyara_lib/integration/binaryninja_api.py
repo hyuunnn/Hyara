144915fa3884b9f0d4e12de437877f837f7b5029
Hyun Yi <hy00un__@naver.com>
2021-01-18 22:13:15 +0900

    Update binaryninja_api.py

--- hyara_lib/integration/binaryninja_api.py
+++ hyara_lib/integration/binaryninja_api.py
@@ -1,36 +1,93 @@
 from ..ui.settings import HyaraGUI
+import pefile
+import hashlib
+import binascii
+
+from binaryninjaui import (
+    DockHandler,
+    DockContextHandler,
+    UIActionHandler,
+    UIAction,
+    Menu,
+    DockHandler,
+)
+
+
+def binja_get_bv_from_dock():
+    # https://github.com/gaasedelen/lighthouse/blob/master/plugins/lighthouse/util/disassembler/binja_api.py#L181
+    dh = DockHandler.getActiveDockHandler()
+    if not dh:
+        return None
+    vf = dh.getViewFrame()
+    if not vf:
+        return None
+    vi = vf.getCurrentViewInterface()
+    bv = vi.getData()
+    return bv
 
 
 class HyaraBinaryNinja(HyaraGUI):
     def __init__(self):
         super(HyaraBinaryNinja, self).__init__()
 
+    @property
+    def bv(self):
+        return binja_get_bv_from_dock()
+
     def get_disasm(self, start_address, end_address) -> list:
-        pass
+        result = []
+        bv = self.bv
+        bv.next_address = start_address
+        while bv.next_address < end_address:
+            result.append(bv.get_next_disassembly())
+        return result
 
     def get_hex(self, start_address, end_address) -> str:
-        pass
+        start = start_address
+        return binascii.hexlify(self.bv.read(start, end_address - start)).decode()
 
     def get_comment_hex(self, start_address, end_address) -> list:
-        pass
+        result = []
+        bv = self.bv
+        bv.next_address = start_address
+        while bv.next_address < end_address:
+            start = bv.next_address
+            bv.get_next_disassembly()
+            end = bv.next_address
+            result.append(self.get_hex(start, end))
+        return result
 
     def get_string(self, start_address, end_address) -> list:
-        pass
+        start = start_address
+        length = end_address - start
+        return [i.value for i in self.bv.get_strings(start, length)]
 
     def get_filepath(self) -> str:
-        pass
+        return self.bv.file.original_filename
 
     def get_md5(self) -> str:
-        pass
+        with open(self.get_filepath(), "rb") as f:
+            return hashlib.md5(f.read()).hexdigest()
 
     def get_imphash(self) -> str:
-        pass
+        return pefile.PE(self.get_filepath()).get_imphash()
 
     def get_rich_header(self) -> str:
-        pass
+        rich_header = pefile.PE(self.get_filepath()).parse_rich_header()
+        return hashlib.md5(rich_header["clear_data"]).hexdigest()
 
     def get_pdb_path(self) -> str:
-        pass
+        # https://github.com/VirusTotal/yara/blob/master/docs/modules/pe.rst
+        pe = pefile.PE(self.get_filepath())
+        rva = pe.OPTIONAL_HEADER.DATA_DIRECTORY[6].VirtualAddress
+        size = pe.OPTIONAL_HEADER.DATA_DIRECTORY[6].Size
+        return (
+            pe.parse_debug_directory(rva, size)[0]
+            .entry.PdbFileName.split(b"\x00", 1)[0]
+            .decode()
+            .replace("\\", "\\\\")
+        )
 
     def jump_to(self, addr):
-        pass
+        bv = self.bv
+        bv.navigate(bv.view, addr)
